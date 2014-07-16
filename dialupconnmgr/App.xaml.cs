4e35ecda7e0d332f5a19508b24cd4051b1f25868
truepele <truepele@gmail.com>
2014-07-16 13:53:41 +0300

    Single instance

--- dialupconnmgr/App.xaml.cs
+++ dialupconnmgr/App.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
@@ -16,12 +18,56 @@ namespace dialupconnmgr
     public partial class App : Application
     {
         private static Window _mainWindow;
+        static Mutex _singleInstMutex;
+        private static EventWaitHandle _singleInstWaithandle;
+        static bool _isMutexCreated;
+        private static string _m_UID;
+        private static string _w_UID;
 
         public App()
         {
-            this.InitializeComponent();
-            _mainWindow = new MainWindow();
-            _mainWindow.Show();
+            const string idword = "dialupconnmgrsynchronisation";
+            _m_UID = Environment.UserName + idword+ "MUTEX";
+            _w_UID = Environment.UserName + idword + "EventWaitHandle";
+
+            _singleInstMutex = new Mutex(true, _m_UID, out _isMutexCreated);
+            _singleInstWaithandle = new EventWaitHandle(false, EventResetMode.AutoReset, _w_UID);
+            if (_isMutexCreated)
+            {
+                this.InitializeComponent();
+                _mainWindow = new MainWindow();
+                _mainWindow.Show();
+
+                Task.Factory.StartNew(async () =>
+                {
+                    while (true)
+                    {
+                        if (_singleInstWaithandle.WaitOne() && _mainWindow != null)
+                        {
+                            await DispatcherInvokeAsync(() =>
+                            {
+                                _mainWindow.Show();
+                                _mainWindow.Activate();
+                            });
+                        }
+                    }
+                });
+            }
+            else
+            {
+                _singleInstWaithandle.Set();
+                Shutdown();
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_isMutexCreated && _singleInstMutex != null)
+            {
+                _singleInstMutex.ReleaseMutex();
+                _singleInstMutex.Dispose();
+                base.OnExit(e);
+            }
         }
 
         public static async Task DispatcherInvokeAsync(Action action)
